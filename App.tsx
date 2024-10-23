c9a63515954d5046e8a44debfe811960fc1efc3f
Kallinikos <kallinikosmil@gmail.com>
2024-10-23 23:52:18 +0300

    is ssh working?

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsxa</Text>
+      <Text>Open up App.tsxas</Text>
       <StatusBar style="auto" />
     </View>
   );
